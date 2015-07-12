8e4420aef93ec26a1b17f9e798202d0b3d1a8c88
Ellen <roster@charter.net>
2015-07-12 15:09:44 -0500

    Improving Plot 1

--- plot1.R
+++ plot1.R
@@ -10,7 +10,7 @@ plot1 <- function() {
     nenergy <- subset(energy, Date == "1/2/2007" | Date == "2/2/2007")
     png(file="./R-work/ExData_Plotting1/plot1.png",width=480,height=480)
     hist(nenergy$Global_active_power, main = "Global Active Power", col="red",xlab="Global Active Power (kilowatts)")
-##    dev.off()
+    dev.off()
 }
 
 
