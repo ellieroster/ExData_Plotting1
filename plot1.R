a2b5fe9676f1eda37b95cb2f483e7a0fa8de99a9
Ellen <roster@charter.net>
2015-07-12 10:20:55 -0500

    Fixed title plot1

--- plot1.R
+++ plot1.R
@@ -1,12 +1,16 @@
 plot1 <- function() {
+  
+## Reads text file of household energy consumption
+## Subsets file to include data from 2/1/2007-2/2/2007
+## Creates .png of histogram of useage of Global Active Power - color is red
+## Creates title for historgram
+## Closes graphic device (.png file)
+
     energy <-read.table("./data/household_power_consumption.txt",sep=";", na.strings = "?", header = TRUE)
-    head (energy)
     nenergy <- subset(energy, Date == "1/2/2007" | Date == "2/2/2007")
-    png(file="plot1.png",width=480,height=480)
-  print("here")
-    hist(nenergy$Global_active_power, col="green",xlab="Global Active Power (kilowatts)")
- title(main = "Global Active Power")
-  dev.off()
+    png(file="./R-work/ExData_Plotting1/plot1.png",width=480,height=480)
+    hist(nenergy$Global_active_power, main = "Global Active Power", col="red",xlab="Global Active Power (kilowatts)")
+    dev.off()
 }
 
 
